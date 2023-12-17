a2723039414a6100d395ab9b86279d3ad99d4b68
omodtung <dothedung131@gmail.com>
2023-12-17 22:05:07 +0700

    option 2 With email + name

--- assets/js/page/signup.js
+++ assets/js/page/signup.js
@@ -1,5 +1,8 @@
 const btnSignUp = document.querySelector(".btn-signup");
 const inputNameSelector = document.querySelector(".name");
+const inputEmailSelector = document.querySelector(".email")
+const inputPassWord  = document.querySelector(".password")
+const inputConfirmPass =document.querySelector(".confirm_password")
 
 // btnSignUp.addEventListener('click',handleSignClick())
 
@@ -13,31 +16,7 @@ const inputNameSelector = document.querySelector(".name");
 
 // Viet code theo Arrow Function syntax
 
-btnSignUp.addEventListener("click", () => {
-  event.preventDefault();
-
-  let inputValueName = inputNameSelector.value;
- 
-  let errorNameSelector = inputNameSelector
-    .closest('.form-group')
-    .querySelector('.error_message');
-    if (inputValueName === "")
-    {
-      inputNameSelector.classList.add('error');
-      errorNameSelector.textContent="Ten Khong Duoc Bo Trong"
-    }
-    else
-    {
-      inputNameSelector.classList.remove('error')
-      errorNameSelector.textContent=""
-
-    }
-});
-
-// //option 2
-// btnSignUp.addEventListener("click", (event) => handleSignClick(event)); // Pass 'event' as argument
-
-// function handleSignClick(event) {
+// btnSignUp.addEventListener("click", () => {
 //   event.preventDefault();
 
 //   let inputValueName = inputNameSelector.value;
@@ -56,6 +35,44 @@ btnSignUp.addEventListener("click", () => {
 //       errorNameSelector.textContent=""
 
 //     }
+// });
 
-    
-// }
+// //option 2
+btnSignUp.addEventListener("click", (event) => handleSignClick(event)); // Pass 'event' as argument
+
+function handleSignClick(event) {
+  event.preventDefault();
+
+  let inputValueName = inputNameSelector.value;
+ 
+  let errorNameSelector = inputNameSelector
+    .closest('.form-group')
+    .querySelector('.error_message');
+    if (inputValueName === "")
+    {
+      inputNameSelector.classList.add('error');
+      errorNameSelector.textContent="Ten Khong Duoc Bo Trong"
+    }
+    else
+    {
+      inputNameSelector.classList.remove('error')
+      errorNameSelector.textContent=""
+
+    }
+
+
+    let inputEmail = inputEmailSelector.value
+    let errorEmailSelector = inputEmailSelector.closest('.form-group').querySelector('.error_message')
+  
+    if (inputEmail === "")
+    {
+      inputEmailSelector.classList.add('error');
+  errorEmailSelector.textContent="THieu Emial"
+    }
+    else
+    {
+      inputEmailSelector.classList.remove('error')
+      errorEmailSelector.textContent=""
+
+    }
+}
